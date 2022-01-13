52489b30dda5378c819ce32fbbee7de7ef741b7f
Trung Kien <kien123456k@gmail.com>
2022-01-14 01:44:55 +0700

    SEEM-15 refactor App.js and index.js

--- SEEMS/Client/src/index.js
+++ SEEMS/Client/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-
-import { ThemeProvider } from '@mui/material'
 
 import App from './App'
-import store from './App/store'
 import './index.css'
-import seemsTheme from './theme/seemsTheme'
 
 ReactDOM.render(
     <React.StrictMode>
-        <ThemeProvider theme={seemsTheme}>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </ThemeProvider>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 )
