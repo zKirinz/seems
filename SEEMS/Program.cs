fa535650eaf7756b9bf5f837ea9dd92256f14845
Trung Kien <kien123456k@gmail.com>
2022-01-13 13:55:02 +0700

    SEEM-24 add Swagger for API document

--- SEEMS/Program.cs
+++ SEEMS/Program.cs
@@ -3,19 +3,20 @@ using SEEMS.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection"));
 });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseStaticFiles();
