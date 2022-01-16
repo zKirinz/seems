0e4198ec2a20ef0a9db4667d83eadedf3b91c8e9
Hien-BT01 <hienbui25g@gmail.com>
2022-01-16 11:55:11 +0700

    SEEM-18 implement header introduction

---
+++ SEEMS/Client/src/components/Header/Introduction.js
@@ -0,0 +1,79 @@
+import { Box, Typography } from '@mui/material'
+
+const Introduction = ({ src, size }) => {
+    return (
+        <Box
+            sx={{
+                marginTop: (theme) => theme.spacing(10),
+                height: '400px',
+                backgroundImage: `url(${src})`,
+                width: '100%',
+                backgroundPosition: 'center',
+                backgroundSize: size,
+                backgroundRepeat: 'no-repeat',
+            }}
+            position="relative"
+        >
+            <Box
+                sx={{
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    bottom: 0,
+                    backgroundColor: 'rgba(0,0,0,.7)',
+                    zIndex: 1,
+                }}
+                position="absolute"
+            ></Box>
+            <Box
+                position="absolute"
+                sx={{
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    zIndex: 3,
+                    py: 5,
+                    px: 10,
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <Typography
+                        component="h1"
+                        sx={{
+                            fontSize: { xs: '64px', md: '90px' },
+                            fontWeight: '900',
+                            letterSpacing: (theme) => theme.spacing(1),
+                            mt: { xs: '24px' },
+                        }}
+                        color="secondary"
+                    >
+                        SEEMS
+                    </Typography>
+                    <Typography
+                        paragraph
+                        color="secondary"
+                        sx={{
+                            fontSize: { xs: '16px', sm: '24px' },
+                            maxWidth: '800px',
+                            mt: (theme) => theme.spacing(3),
+                        }}
+                        align="center"
+                    >
+                        Nơi các bạn trẻ sinh viên có thể tìm kiếm các sự kiện vui nhộn, giải trí,
+                        hấp dẫn kinh doanh và học thuật thuộc vấn đề tài Software.
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default Introduction
--- SEEMS/Client/src/pages/HomePage.js
+++ SEEMS/Client/src/pages/HomePage.js
@@ -1,17 +1,18 @@
 import { Fragment } from 'react'
 
 import Header from '../components/Header'
-import { Box, Typography } from '@mui/material'
+import Introduction from '../components/Header/Introduction'
+
+const introHeaderImage = {
+    src: 'https://www.fpt-software.com/wp-content/uploads/sites/2/2020/02/Logistech-social-media-post-2-1024x536.png',
+    size: '100% 100%',
+}
 
 const HomePage = () => {
     return (
         <Fragment>
             <Header />
-            <Box>
-                <Box component="div">
-                    <Typography></Typography>
-                </Box>
-            </Box>
+            <Introduction src={introHeaderImage.src} size={introHeaderImage.size} />
         </Fragment>
     )
 }
