4fc2700e140b493f711895da4cabb6f6d39b967d
Hien-BT01 <hienbui25g@gmail.com>
2022-01-17 17:01:57 +0700

    SEEM-18 implement gallery in home page

--- SEEMS/Client/src/pages/HomePage/CurrentEvent.js
+++ SEEMS/Client/src/pages/HomePage/CurrentEvent.js
@@ -1,118 +1,133 @@
-import { Box, Avatar, Card, CardContent, Grid, IconButton, Typography } from '@mui/material'
+import React from 'react'
+
+import TimeText from '../../components/TimeText/Index'
+import { Box, Avatar, Card, CardContent, Grid, IconButton, Typography, Paper } from '@mui/material'
+
+import TitleHomePage from './TitleHomePage'
 
 const CurrentEvent = ({ currentEvents }) => {
     return (
-        <Box sx={{ px: 5, mt: 2 }}>
-            {currentEvents.map((currentEvent) => (
-                <Grid container mt={4} key={currentEvent.id}>
-                    <Grid item sm={12} xs={12} md={4}>
-                        <Box sx={{ backgroundColor: 'secondary.light', pt: 3 }}>
-                            <Typography
-                                align="center"
-                                sx={{
-                                    fontWeight: (theme) => theme.typography.fontWeightBold,
-                                }}
-                                color="primary.contrastText"
-                                variant="h5"
-                            >
-                                {currentEvent.time}
-                            </Typography>
-                            <Typography
-                                align="center"
-                                color="primary.contrastText"
-                                fontWeight={(theme) => theme.typography.fontWeightMedium}
-                            >
-                                Room No - <strong>{currentEvent.roomNum}</strong>
-                            </Typography>
-                            <Box display="flex" alignItems="center">
-                                <Avatar
-                                    src="https://demo-egenslab.b-cdn.net/html/eventlab/assets/images/speaker/speaker-sm2.png"
-                                    alt="person"
-                                    sx={{
-                                        width: '171px',
-                                        height: '168px',
-                                        overflow: 'unset',
-                                    }}
-                                ></Avatar>
-                                <Box ml={3}>
-                                    <Typography
-                                        mb={1}
-                                        color="primary.contrastText"
-                                        fontWeight={(theme) => theme.typography.fontWeightBold}
-                                        sx={{ fontSize: { sm: 12, md: 14, lg: 18 } }}
-                                    >
-                                        {currentEvent.speaker}
-                                    </Typography>
-                                    <Typography
-                                        color="primary.contrastText"
-                                        fontWeight={(theme) => theme.typography.fontWeightMedium}
-                                        letterSpacing={1}
-                                        variant="h6"
-                                        fontSize={{
-                                            xs: 12,
-                                            sm: 14,
-                                            lg: 16,
+        <React.Fragment>
+            <TitleHomePage title="Event" variant="h4" color="secondary" align="center" />
+            <TimeText variant="h3" color="#ce1446" time="16/01/2022" component="h1" stroke={true} />
+            <Box>
+                {currentEvents.map((currentEvent) => (
+                    <Grid
+                        container
+                        mt={4}
+                        key={currentEvent.id}
+                        component={Paper}
+                        elevation={2}
+                        overflow="hidden"
+                    >
+                        <Grid item sm={12} xs={12} md={4}>
+                            <Box sx={{ backgroundColor: 'secondary.light', pt: 3 }}>
+                                <TimeText
+                                    color="primary.contrastText"
+                                    variant="h5"
+                                    time={currentEvent.time}
+                                    stroke={false}
+                                    component="h1"
+                                />
+                                <Typography
+                                    align="center"
+                                    color="primary.contrastText"
+                                    fontWeight={(theme) => theme.typography.fontWeightMedium}
+                                >
+                                    Room No - <strong>{currentEvent.roomNum}</strong>
+                                </Typography>
+                                <Box display="flex" alignItems="center" mt={2}>
+                                    <Avatar
+                                        src="https://demo-egenslab.b-cdn.net/html/eventlab/assets/images/speaker/speaker-sm2.png"
+                                        alt="person"
+                                        sx={{
+                                            width: '171px',
+                                            height: '168px',
+                                            overflow: 'unset',
                                         }}
-                                    >
-                                        {currentEvent.major}
-                                    </Typography>
-                                    <IconButton></IconButton>
+                                    ></Avatar>
+                                    <Box ml={3}>
+                                        <Typography
+                                            mb={1}
+                                            color="primary.contrastText"
+                                            fontWeight={(theme) => theme.typography.fontWeightBold}
+                                            sx={{ fontSize: { sm: 12, md: 14, lg: 18 } }}
+                                        >
+                                            {currentEvent.speaker}
+                                        </Typography>
+                                        <Typography
+                                            color="primary.contrastText"
+                                            fontWeight={(theme) =>
+                                                theme.typography.fontWeightMedium
+                                            }
+                                            letterSpacing={1}
+                                            variant="h6"
+                                            fontSize={{
+                                                xs: 12,
+                                                sm: 14,
+                                                lg: 16,
+                                            }}
+                                        >
+                                            {currentEvent.major}
+                                        </Typography>
+                                        <IconButton></IconButton>
+                                    </Box>
                                 </Box>
                             </Box>
-                        </Box>
+                        </Grid>
+                        <Grid item sm={12} xs={12} md={8}>
+                            <Card sx={{ height: '100%' }}>
+                                <CardContent>
+                                    <Typography
+                                        component="h1"
+                                        fontWeight={700}
+                                        color="secondary"
+                                        sx={{ fontSize: { md: 20, lg: 24, sm: 18 } }}
+                                    >
+                                        {currentEvent.title}
+                                    </Typography>
+                                    <Typography
+                                        component="p"
+                                        mt={2}
+                                        sx={{ color: '#515154' }}
+                                        fontWeight={500}
+                                    >
+                                        {currentEvent.content}
+                                    </Typography>
+                                    <Typography
+                                        mt={1}
+                                        component="h5"
+                                        color="secondary"
+                                        fontWeight={700}
+                                        sx={{ textDecoration: 'underline' }}
+                                    >
+                                        Topic
+                                    </Typography>
+                                    <Grid mt={2} container>
+                                        {currentEvent.topics.map((topic) => (
+                                            <Grid key={topic.id} xs={12} sm={4} item>
+                                                <Typography
+                                                    fontSize={{
+                                                        xs: 14,
+                                                        sm: 18,
+                                                    }}
+                                                    sx={{
+                                                        color: (theme) => theme.palette.grey[800],
+                                                        textAlign: { xs: 'left', sm: 'center' },
+                                                    }}
+                                                >
+                                                    {topic.title}
+                                                </Typography>
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </CardContent>
+                            </Card>
+                        </Grid>
                     </Grid>
-                    <Grid item sm={12} xs={12} md={8}>
-                        <Card sx={{ height: '100%' }}>
-                            <CardContent>
-                                <Typography
-                                    component="h1"
-                                    fontWeight={700}
-                                    color="secondary"
-                                    sx={{ fontSize: { md: 20, lg: 24, sm: 18 } }}
-                                >
-                                    {currentEvent.title}
-                                </Typography>
-                                <Typography
-                                    component="p"
-                                    mt={2}
-                                    sx={{ color: '#515154' }}
-                                    fontWeight={500}
-                                >
-                                    {currentEvent.content}
-                                </Typography>
-                                <Typography
-                                    component="h5"
-                                    color="secondary"
-                                    mt={3}
-                                    fontWeight={700}
-                                    sx={{ textDecoration: 'underline' }}
-                                >
-                                    Topic
-                                </Typography>
-                                <Grid mt={2} container>
-                                    {currentEvent.topics.map((topic) => (
-                                        <Grid key={topic.id} xs={12} sm={4} item>
-                                            <Typography
-                                                fontSize={{
-                                                    xs: 14,
-                                                    sm: 18,
-                                                }}
-                                                sx={{
-                                                    color: (theme) => theme.palette.grey[800],
-                                                    textAlign: { xs: 'left', sm: 'center' },
-                                                }}
-                                            >
-                                                {topic.title}
-                                            </Typography>
-                                        </Grid>
-                                    ))}
-                                </Grid>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
-            ))}
-        </Box>
+                ))}
+            </Box>
+        </React.Fragment>
     )
 }
 
--- SEEMS/Client/src/pages/HomePage/index.js
+++ SEEMS/Client/src/pages/HomePage/index.js
@@ -2,9 +2,10 @@ import { Fragment } from 'react'
 
 import Header from '../../components/Header'
 import Introduction from '../../components/Header/Introduction'
-import { Box, Typography } from '@mui/material'
+import { Box } from '@mui/material'
 
 import CurrentEvent from './CurrentEvent'
+import Gallery from './Gallery'
 
 const introHeaderImage = {
     title: 'SEEMS',
@@ -12,6 +13,21 @@ const introHeaderImage = {
     src: 'https://media-exp1.licdn.com/dms/image/C5616AQEMxYQLodosUA/profile-displaybackgroundimage-shrink_200_800/0/1630122993980?e=1648080000&v=beta&t=qRUkeRz5I8u5kb9NweehGZPucpXkmJ6FIEwPCfdZ0Os',
     size: '100% 100%',
 }
+
+const gallery = [
+    {
+        src: 'https://res.cloudinary.com/dq7l8216n/image/upload/v1642134779/Telescope.jpg',
+        size: 'cover',
+    },
+    {
+        src: 'https://res.cloudinary.com/dq7l8216n/image/upload/v1642134779/Telescope.jpg',
+        size: 'cover',
+    },
+    {
+        src: 'https://res.cloudinary.com/dq7l8216n/image/upload/v1642134779/Telescope.jpg',
+        size: 'cover',
+    },
+]
 
 const paths = [
     {
@@ -61,6 +77,30 @@ const currentEvents = [
             },
         ],
     },
+    {
+        id: 1,
+        speaker: 'Joseph John',
+        time: '10.00 AM - 11.30PM',
+        roomNum: '01',
+        major: 'Management',
+        title: 'Nulla nisl tellus hendrerit nec dignissim pellentesqu posu in est Suspendisse ',
+        content:
+            'Cras semper, massa vel aliquam luctus, eros odio tempor turpis, ac placerat metus tortor eget magna. Donec mattis posuere pharetra. Donec vestibulum ornare velit ut sollicitudin ut sollicitudin.',
+        topics: [
+            {
+                id: 'GraphicD',
+                title: 'Graphic Design',
+            },
+            {
+                id: 'Dev',
+                title: 'Development',
+            },
+            {
+                id: 'WebD',
+                title: 'Web Design',
+            },
+        ],
+    },
 ]
 
 const HomePage = () => {
@@ -75,44 +115,10 @@ const HomePage = () => {
             />
             <Box
                 component="main"
-                sx={{ paddingTop: (theme) => theme.spacing(10), backgroundColor: '#fafafa' }}
+                sx={{ paddingTop: (theme) => theme.spacing(10), backgroundColor: '#fafafa', px: 5 }}
             >
-                <Typography
-                    color="secondary"
-                    align="center"
-                    sx={{
-                        fontWeight: (theme) => theme.typography.fontWeightBold,
-                    }}
-                    variant="h4"
-                    mb={1}
-                >
-                    Event
-                </Typography>
-                <Typography
-                    align="center"
-                    sx={{
-                        color: (theme) => theme.palette.grey[800],
-                        fontWeight: (theme) => theme.typography.fontWeightBold,
-                        textTransform: 'uppercase',
-                    }}
-                    variant="h4"
-                >
-                    lịch các sự kiện hiện tại
-                </Typography>{' '}
-                <Typography
-                    component="h1"
-                    sx={{
-                        fontWeight: (theme) => theme.typography.fontWeightBold,
-                        '-webkit-text-stroke': '1px #ce1446',
-                        '-webkit-text-fill-color': 'transparent',
-                    }}
-                    align="center"
-                    variant="h3"
-                    mt={1}
-                >
-                    16/01/2022
-                </Typography>
                 <CurrentEvent currentEvents={currentEvents} />
+                <Gallery gallery={gallery} />
             </Box>
         </Fragment>
     )
