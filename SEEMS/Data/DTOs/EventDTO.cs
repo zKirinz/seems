121b5e42de8c5a9b674b77b36e367a7474e68b12
Thinh Le <letienthinh0109@gmail.com>
2022-01-18 20:22:42 +0700

    Seem-25 Finish api/add-event endpoint

--- SEEMS/Data/DTOs/EventDTO.cs
+++ SEEMS/Data/DTOs/EventDTO.cs
@@ -1,26 +1,31 @@
-﻿namespace SEEMS.Data.DTO
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SEEMS.Data.DTO
 {
     public class EventDTO
     {
+        [Required]
+        public string EventTitle { get; set; }
 
-        public String EventTitle { get; set; }
+        [Required]
+        public string EventDescription { get; set; }
 
-        public String EventDescription { get; set; }
-
+        [JsonIgnore]
         public int? ChainOfEventId { get; set; }
-
+        [Required]
         public bool IsPrivate { get; set; }
-
+        [Required]
         public String ImageUrl { get; set; }
-
+        [Required]
         public decimal ExpectPrice { get; set; }
-
+        [Required]
         public bool Active { get; set; }
-
-        public String Location { get; set; }
-
+        [Required]
+        public string Location { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
-
+        [Required]
         public DateTime EndDate { get; set; }
 
     }
