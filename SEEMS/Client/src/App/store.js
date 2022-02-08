660978585672293c1a9fcc4e9f838483381ba3cf
Trung Kien <kien123456k@gmail.com>
2022-02-08 23:48:17 +0700

    SEEM-75 change Redux Toolkit to Recoil state management

--- SEEMS/Client/src/App/store.js
+++
@@ -1,9 +0,0 @@
-import authReducer from '../features/auth/authSlice'
-
-import { configureStore } from '@reduxjs/toolkit'
-
-export default configureStore({
-    reducer: {
-        auth: authReducer,
-    },
-})
