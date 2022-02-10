012e359c08536fef1e30c98c857e7d7625379467
Le Tien Thinh <59760568+thinhle19@users.noreply.github.com>
2022-02-10 21:38:09 +0700

    Seem 37 implements api to get all events (#28)
    * WIP Event Controller
    
    * Very first custom JsendResponse
    
    * custom response,raw get & post events
    
    * Modify format & fix EventController

--- SEEMS/Controllers/EventController.cs
+++ SEEMS/Controllers/EventController.cs
@@ -1,55 +1,101 @@
 ﻿using AutoMapper;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
 using SEEMS.Contexts;
 using SEEMS.Data.DTO;
 using SEEMS.Models;
+using SEEMS.Services;
 
 namespace SEEMS.Controller
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [ApiExplorerSettings(GroupName = "v1")]
-    public class EventController : ControllerBase
+	[Route("api/[controller]")]
+	[ApiController]
+	[ApiExplorerSettings(GroupName = "v1")]
+	public class EventController : ControllerBase
 
-    {
-        private readonly ApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        public EventController(ApplicationDbContext context, IMapper mapper)
-        {
-            this._context = context;
-            this._mapper = mapper;
-        }
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly IMapper _mapper;
+		public EventController(ApplicationDbContext context, IMapper mapper)
+		{
+			this._context = context;
+			this._mapper = mapper;
+		}
 
-        [HttpPost("add-event")]
-        public IActionResult Post([FromBody] EventDTO eventDTO)
-        {
-            //try
-            //{
-            if (!ModelState.IsValid)
-            {
-                //throw new InvalidOperationException();
-                return BadRequest();
-            }
-            else
-            {
-                var _event = _mapper.Map<Event>(eventDTO);
-                _context.Events.Add(_event);
-                _context.SaveChanges();
-                return Ok(eventDTO);
-            }
-            //}
-            //catch (Exception ex)
-            //{
-            //}
+		/*private static List<Event> events = new List<Event>
+				{
+				 new Event{
+					  EventTitle = "Tech Talk",
+					 EventDescription = "This tech talk description",
+					 IsPrivate = true,
+					 ImageUrl = "this",
+					 ExpectPrice = 5,
+					 Active = true,
+					 Location = "FPTU",
+					 StartDate = DateTime.Now,
+					 EndDate = DateTime.Now,
+				 },
+			 new Event{
+					 EventTitle = "Tech Talk 2",
+					 EventDescription = "This tech talk description",
+					 IsPrivate = true,
+					 ImageUrl = "this",
+					 ExpectPrice = 5,
+					 Active = true,
+					 Location = "FPTU",
+					 StartDate = DateTime.Now,
+					 EndDate = DateTime.Now,
+				 }
+			 };*/
 
-        }
+		[HttpGet()]
+		public async Task<ActionResult<List<Event>>> Get()
+		{
+			try
+			{
 
-        [HttpGet()]
-        public IActionResult Get()
-        {
-            return Ok();
-        }
-    }
+				return Ok(new Response(ResponseStatusEnum.Success, _context.Events.ToList()));
+			}
+			catch (Exception ex)
+			{
+				return Ok(new Response(ResponseStatusEnum.Error, ex.Message));
+			}
+		}
+
+		//[HttpGet("{id}")]
+		//public async Task<ActionResult<Event>> Get(int id)
+		//{
+		//need to replace by EF 
+		//var anEvent = events.Find(h => h.Id == id);
+		//if (anEvent == null)
+		//    return BadRequest("Event not found.");
+		//return Ok(anEvent);
+		//}
+
+		[HttpPost]
+		public async Task<ActionResult> AddEvent(EventDTO anEvent)
+		{
+			try
+			{
+				if (anEvent.EventTitle == "3")
+				{
+					return BadRequest(new Response(ResponseStatusEnum.Fail, new { Title = "Id cannot null" }));
+				}
+				else
+				{
+					_context.Events.Add(_mapper.Map<Event>(anEvent));
+					_context.SaveChanges();
+					return Ok(new Response(ResponseStatusEnum.Success, _context.Events.ToList()));
+				}
+			}
+			catch (Exception ex)
+			{
+				return Ok(new Response(ResponseStatusEnum.Error, ex.Message));
+			}
+		}
+
+	}
 }
