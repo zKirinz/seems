6a0afb5a91eb0ef4656fade08ac125cfc0721115
niinkoi <nkhoinguyen26@gmail.com>
2022-02-10 23:22:39 +0700

    Add missing meta

---
+++ SEEMS/Data/Repositories/Implements/UserMetaRepository.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using SEEMS.Contexts;
+using SEEMS.Data.Models;
+using SEEMS.Infrastructures.Commons;
+using SEEMS.Models;
+
+namespace SEEMS.Data.Repositories.Implements;
+
+public class UserMetaRepository : RepositoryBase<UserMeta>, IUserMetaRepository
+{
+    public UserMetaRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+
+    public void CreateMeta(UserMeta meta) => Create(meta);
+    
+    public void RegisterRole(User user, string role)
+    {
+        UserMeta roleMeta = new UserMeta();
+        roleMeta.User = user;
+        roleMeta.MetaKey = "role";
+        roleMeta.MetaValue = role;
+        
+        CreateMeta(roleMeta);
+    }
+
+    public async Task<UserMeta> GetRolesAsync(string email, bool trackChanges) =>
+        await FindByCondition(m => m.User.Email.Equals(email), trackChanges)
+            .SingleOrDefaultAsync();
+
+}
