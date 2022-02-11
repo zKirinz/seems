43a5b6ae5051321d6d8a64c35e3b759f5d12fb6a
Thinh Le <letienthinh0109@gmail.com>
2022-02-11 23:25:41 +0700

    SEEM-85 Validation for Create Event API

--- SEEMS/Controllers/EventController.cs
+++ SEEMS/Controllers/EventController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 using SEEMS.Contexts;
 using SEEMS.Data.DTO;
+using SEEMS.Data.ValidationInfo;
 using SEEMS.Models;
 using SEEMS.Services;
 
@@ -19,43 +20,44 @@ namespace SEEMS.Controller
 	{
 		private readonly ApplicationDbContext _context;
 		private readonly IMapper _mapper;
-		private EventsServices _eventsServices;
-		public EventController()
+		public EventController(ApplicationDbContext context, IMapper mapper)
 		{
-			_eventsServices = new EventsServices(_context, _mapper);
+			this._mapper = mapper;
+			this._context = context;
 		}
 
-		//[HttpGet()]
-		//public async Task<ActionResult<List<Event>>> Get()
-		//{
-		//	try
-		//	{
-		//		return Ok(new Response(ResponseStatusEnum.Success, _context.Events.ToList()));
-		//	}
-		//	catch (Exception ex)
-		//	{
-		//		return Ok(new Response(ResponseStatusEnum.Error, ex.Message));
-		//	}
-		//}
+		/*[HttpGet()]
+		public async Task<ActionResult<List<Event>>> Get()
+		{
+			try
+			{
+				return Ok(new Response(ResponseStatusEnum.Success, _context.Events.ToList()));
+			}
+			catch (Exception ex)
+			{
+				return Ok(new Response(ResponseStatusEnum.Error, ex.Message));
+			}
+		}
 
-		//[HttpGet("{id}")]
-		//public async Task<ActionResult<Event>> Get(int id)
-		//{
-		//need to replace by EF 
-		//var anEvent = events.Find(h => h.Id == id);
-		//if (anEvent == null)
-		//    return BadRequest("Event not found.");
-		//return Ok(anEvent);
-		//}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Event>> Get(int id)
+		{
+			//need to replace by EF
+			var anEvent = events.Find(h => h.Id == id);
+			if (anEvent == null)
+				return BadRequest("Event not found.");
+			return Ok(anEvent);
+		}*/
 
 		[HttpPost]
 		public async Task<ActionResult> AddEvent(EventDTO anEvent)
 		{
+			EventValidationInfo eventValidationInfo = EventsServices.GetValidatedEventInfo(anEvent);
 			try
 			{
-				if (anEvent.EventTitle == "3")
+				if (eventValidationInfo != null)
 				{
-					return BadRequest(new Response(ResponseStatusEnum.Fail, new { Title = "Id cannot null" }));
+					return BadRequest(new Response(ResponseStatusEnum.Fail, eventValidationInfo));
 				}
 				else
 				{
@@ -66,9 +68,8 @@ namespace SEEMS.Controller
 			}
 			catch (Exception ex)
 			{
-				return Ok(new Response(ResponseStatusEnum.Error, ex.Message));
+				return StatusCode(StatusCodes.Status500InternalServerError, eventValidationInfo);
 			}
 		}
-
 	}
 }
--- SEEMS/Services/EventsServices.cs
+++ SEEMS/Services/EventsServices.cs
@@ -1,19 +1,43 @@
-﻿using AutoMapper;
-
-using SEEMS.Contexts;
+﻿using SEEMS.Data.DTO;
+using SEEMS.Data.ValidationInfo;
 
 namespace SEEMS.Services
 {
 	public class EventsServices
 	{
-		private readonly ApplicationDbContext _context;
-		private readonly IMapper _mapper;
-
-		public EventsServices(ApplicationDbContext context, IMapper mapper)
+		public static EventValidationInfo GetValidatedEventInfo(EventDTO eventDTO)
 		{
-			_context = context;
-			_mapper = mapper;
+			EventValidationInfo validationInfo = new EventValidationInfo();
+			bool failedCheck = false;
+			if (eventDTO.EventTitle.Length < EventValidationInfo.MinTitleLength ||
+				eventDTO.EventTitle.Length > EventValidationInfo.MaxTitleLength)
+			{
+				failedCheck = true;
+				validationInfo.Title = $"Event title must from {EventValidationInfo.MinTitleLength} to {EventValidationInfo.MaxTitleLength} length";
+			}
+			if (eventDTO.EventDescription.Length < EventValidationInfo.MinDescriptionLength ||
+				eventDTO.EventDescription.Length > EventValidationInfo.MaxDescriptionLength)
+			{
+				failedCheck = true;
+				validationInfo.Description = $"Event description must from {EventValidationInfo.MinDescriptionLength} to {EventValidationInfo.MaxDescriptionLength} length";
+			}
+			if (eventDTO.Location.Length < EventValidationInfo.MinLocationLength ||
+				eventDTO.Location.Length > EventValidationInfo.MaxLocationLength)
+			{
+				failedCheck = true;
+				validationInfo.Location = $"Event location must from {EventValidationInfo.MinLocationLength} to {EventValidationInfo.MaxLocationLength} length";
+			}
+			if (eventDTO.ExpectPrice < EventValidationInfo.MinPrice)
+			{
+				failedCheck = true;
+				validationInfo.ExpectPrice = $"Price can not smaller than {EventValidationInfo.MinPrice}";
+			}
+			if (eventDTO.EndDate.Subtract(eventDTO.StartDate).Hours < 2)
+			{
+				failedCheck = true;
+				validationInfo.EndDate = $"End time must behind start time";
+			}
+			return failedCheck ? validationInfo : null;
 		}
-
 	}
 }
