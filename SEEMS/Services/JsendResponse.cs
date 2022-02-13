edea388366fc77055f8ce45b7c89d8b5d53cc79c
Thinh Le <letienthinh0109@gmail.com>
2022-02-13 17:08:29 +0700

    SEEM-85 add default value for Code in JsendResponse

--- SEEMS/Services/JsendResponse.cs
+++ SEEMS/Services/JsendResponse.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using Newtonsoft.Json;
+
 using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
 
 namespace SEEMS.Services
 {
@@ -13,22 +16,40 @@ namespace SEEMS.Services
 
 	public class JsendResponse
 	{
+		[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
 		public String Status { get; protected set; }
+		[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
 		public Object Data { get; protected set; }
-
+		[JsonProperty("message", NullValueHandling = NullValueHandling.Ignore)]
 		public String? Message { get; protected set; }
-
+		[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
 		public int? Code { get; protected set; }
 	}
 
 	public class Response : JsendResponse
 	{
-		public Response(ResponseStatusEnum status, Object data, [Optional] String msg, [Optional] int code)
+		public Response(ResponseStatusEnum status, [Optional] Object data, [Optional] String msg, [Optional] int? code)
 		{
 			Status = Enum.GetName(status).ToLower();
 			Data = data;
 			Message = msg;
-			Code = code;
+			if (code == null)
+			{
+				switch (status)
+				{
+					case ResponseStatusEnum.Success:
+						Code = 200;
+						break;
+					case ResponseStatusEnum.Fail:
+						Code = 400;
+						break;
+					case ResponseStatusEnum.Error:
+						Code = 500;
+						break;
+				}
+			}
+			else
+				Code = code;
 		}
 	}
 
