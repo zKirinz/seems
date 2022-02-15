d8f700439ccbeac8c43638d5428372ab078a5097
Thinh Le <letienthinh0109@gmail.com>
2022-02-15 12:51:28 +0700

    SEEM-89 add Utilities

---
+++ SEEMS/Services/Utilitiies.cs
@@ -0,0 +1,10 @@
+﻿namespace SEEMS.Services
+{
+	public class Utilitiies
+	{
+		public static bool isAfterMinutes(DateTime src, DateTime des, int minutes)
+		{
+			return src.Subtract(des).TotalMinutes >= minutes;
+		}
+	}
+}
