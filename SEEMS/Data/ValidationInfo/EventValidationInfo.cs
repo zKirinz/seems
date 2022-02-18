c0309ce7ab3ab9ce8498e10fb0347f4ab1540c0d
Thinh Le <letienthinh0109@gmail.com>
2022-02-18 21:21:14 +0700

    Edit validation msg

--- SEEMS/Data/ValidationInfo/EventValidationInfo.cs
+++ SEEMS/Data/ValidationInfo/EventValidationInfo.cs
@@ -5,7 +5,7 @@
 		public const int MinTitleLength = 5;
 		public const int MaxTitleLength = 50;
 		public const int MinDescriptionLength = 20;
-		public const int MaxDescriptionLength = 100;
+		public const int MaxDescriptionLength = 2000;
 		public const int MinPrice = 1000;
 		public const int MinLocationLength = 5;
 		public const int MaxLocationLength = 50;
