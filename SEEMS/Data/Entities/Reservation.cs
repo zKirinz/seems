6792af0bb2051f7c607ec0b984d11193c144517b
niinkoi <nkhoinguyen26@gmail.com>
2022-02-19 18:09:08 +0700

    Update Database schema

--- SEEMS/Data/Entities/Reservation.cs
+++ SEEMS/Data/Entities/Reservation.cs
@@ -2,18 +2,13 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using SEEMS.Data.Models;
 
 namespace SEEMS.Models
 {
     public class Reservation : AbstractEntity<int>
     {
         public bool Attend { get; set; }
-
-        [Column(TypeName = "decimal(7,2)")]
-        public decimal DiscountPercent { get; set; }
-
-        [Column(TypeName = "decimal(7,2)")]
-        public decimal TotalPrice { get; set; }
 
         [ForeignKey("Event")]
         public int EventId { get; set; }
