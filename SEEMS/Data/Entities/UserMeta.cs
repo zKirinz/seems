e834056fe62f4a93e7472517b6edf5f9411f6b29
Thinh Le <letienthinh0109@gmail.com>
2022-02-20 15:58:49 +0700

    SEEM-28 validate just Organizer who created event can update event

--- SEEMS/Data/Entities/UserMeta.cs
+++ SEEMS/Data/Entities/UserMeta.cs
@@ -1,19 +1,23 @@
 ﻿
 using SEEMS.Data.Entities;
 using SEEMS.Data.Models;
+
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace SEEMS.Models
 {
-    public class UserMeta : AbstractEntity<int>
-    {
-        public String MetaKey { get; set; }
+	public class UserMeta : AbstractEntity<int>
+	{
+		public string MetaKey { get; set; }
 
-        public String MetaValue { get; set; }
+		public string MetaValue { get; set; }
 
-        [JsonIgnore]
-        public User? User { get; set; }
-    }
+		[ForeignKey("User")]
+		public int UserId { get; set; }
+
+		[JsonIgnore]
+		public User? User { get; set; }
+	}
 }
