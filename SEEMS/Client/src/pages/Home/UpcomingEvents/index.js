ae31eb53c67c9aff9c61259af490584cfa622fc6
Trung Kien <kien123456k@gmail.com>
2022-02-21 09:41:34 +0700

    SEEM-27 front-end show text when there is not any upcoming events

--- SEEMS/Client/src/pages/Home/UpcomingEvents/index.js
+++ SEEMS/Client/src/pages/Home/UpcomingEvents/index.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react'
 
-import { useHistory } from 'react-router-dom'
+import { Link as RouterLink, useHistory } from 'react-router-dom'
 
 import EventPoster from '../../../components/EventPoster'
 import SearchField from '../../../components/SearchField'
-import { Grid, Card, Typography, Box, Button, useMediaQuery, useTheme } from '@mui/material'
+import { EventBusy as EventBusyIcon } from '@mui/icons-material'
+import {
+    Grid,
+    Card,
+    Typography,
+    Box,
+    Button,
+    useMediaQuery,
+    useTheme,
+    Alert,
+    Link,
+} from '@mui/material'
 
 import useEventAction from '../../../recoil/event/action'
 import EventSummaryInfo from './EventSummaryInfo'
@@ -29,61 +40,77 @@ const UpComingEvents = () => {
             <Typography variant="h3" color="primary" align="center" mt={2} fontWeight={700}>
                 Upcoming Events
             </Typography>
-            <Box mx={{ sx: 2, md: 10 }} my={5}>
-                <SearchField />
-            </Box>
-            <Grid container rowGap={6} display="flex" justifyContent="center">
-                {upcomingEvents.map(({ id, eventTitle, eventDescription, startDate, imageUrl }) => (
-                    <Card
-                        key={id}
-                        elevation={3}
-                        sx={{
-                            position: 'relative',
-                            mx: { sx: 2, md: 10, maxWidth: 1000, width: '100%' },
-                        }}
-                    >
-                        <Box px={{ xs: 2, sm: 4 }} mb={{ xs: 8, md: 4, lg: 0 }}>
-                            <Grid item xs={12} container>
-                                <Grid item xs={12} sm={6} md={4}>
-                                    <EventPoster src={imageUrl} size="contain" />
-                                </Grid>
-                                <Grid item xs={12} sm={6} md={8}>
-                                    <EventSummaryInfo
-                                        title={eventTitle}
-                                        content={eventDescription}
-                                        startTime={startDate}
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Box>
-                        <Box
-                            position="absolute"
-                            bottom={30}
-                            right={matches ? 50 : 10}
-                            display="flex"
-                            justifyContent="center"
-                        >
-                            <Button
-                                variant="contained"
-                                size="large"
-                                onClick={() => history.push(`/events/register/${id}`)}
-                                sx={{ mx: 1 }}
-                            >
-                                Register
-                            </Button>
-                            <Button
-                                variant="contained"
-                                color="secondary"
-                                size="large"
-                                onClick={() => history.push(`/events/${id}`)}
-                                sx={{ mx: 1 }}
-                            >
-                                Read More
-                            </Button>
-                        </Box>
-                    </Card>
-                ))}
-            </Grid>
+            {upcomingEvents.length ? (
+                <React.Fragment>
+                    <Box mx={{ sx: 2, md: 10 }} my={5}>
+                        <SearchField />
+                    </Box>
+                    <Grid container rowGap={6} display="flex" justifyContent="center">
+                        {upcomingEvents.map(
+                            ({ id, eventTitle, eventDescription, startDate, imageUrl }) => (
+                                <Card
+                                    key={id}
+                                    elevation={3}
+                                    sx={{
+                                        position: 'relative',
+                                        mx: { sx: 2, md: 10, maxWidth: 1000, width: '100%' },
+                                    }}
+                                >
+                                    <Box px={{ xs: 2, sm: 4 }} mb={{ xs: 8, md: 4, lg: 0 }}>
+                                        <Grid item xs={12} container>
+                                            <Grid item xs={12} sm={6} md={4}>
+                                                <EventPoster src={imageUrl} size="contain" />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6} md={8}>
+                                                <EventSummaryInfo
+                                                    title={eventTitle}
+                                                    content={eventDescription}
+                                                    startTime={startDate}
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                    </Box>
+                                    <Box
+                                        position="absolute"
+                                        bottom={30}
+                                        right={matches ? 50 : 10}
+                                        display="flex"
+                                        justifyContent="center"
+                                    >
+                                        <Button
+                                            variant="contained"
+                                            size="large"
+                                            onClick={() => history.push(`/events/register/${id}`)}
+                                            sx={{ mx: 1 }}
+                                        >
+                                            Register
+                                        </Button>
+                                        <Button
+                                            variant="contained"
+                                            color="secondary"
+                                            size="large"
+                                            onClick={() => history.push(`/events/${id}`)}
+                                            sx={{ mx: 1 }}
+                                        >
+                                            Read More
+                                        </Button>
+                                    </Box>
+                                </Card>
+                            )
+                        )}
+                    </Grid>
+                </React.Fragment>
+            ) : (
+                <Box display="flex" justifyContent="center" mt={8}>
+                    <Alert icon={<EventBusyIcon />} variant="outlined" severity="warning">
+                        There is not any upcoming events here, let&apos;s{' '}
+                        <RouterLink to="/events/create">
+                            <Link component="span">create one!</Link>
+                        </RouterLink>
+                    </Alert>
+                </Box>
+            )}
+
             <Box display="flex" justifyContent="center" mt={6}>
                 <Button variant="contained" size="large" onClick={() => history.push('/events')}>
                     See All Events
