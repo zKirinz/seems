3fdc200bb06f3370466b0ae80e5422ac835ba3ec
niinkoi <nkhoinguyen26@gmail.com>
2022-02-24 22:30:32 +0700

    Update role only for user and organizer

--- SEEMS/Controllers/admin/UserController.cs
+++ SEEMS/Controllers/admin/UserController.cs
@@ -1,7 +1,10 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
+using SEEMS.Data.DTOs;
 using SEEMS.Data.Entities.RequestFeatures;
 using SEEMS.Data.Models;
 using SEEMS.Infrastructures.Attributes;
@@ -17,11 +20,13 @@ public class UserController : ControllerBase
 {
     private IAuthManager _authManager;
     private IRepositoryManager _repoManager;
+    private IMapper _mapper;
 
-    public UserController(IAuthManager authManager, IRepositoryManager repoManager)
+    public UserController(IAuthManager authManager, IRepositoryManager repoManager, IMapper mapper)
     {
         _authManager = authManager;
         _repoManager = repoManager;
+        _mapper = mapper;
     }
     
     [RoleBasedAuthorization(RoleBased = RoleTypes.ADM)]
@@ -31,5 +36,31 @@ public class UserController : ControllerBase
         var listUsers = await _repoManager.User.GetAllUsersAsync(userParams, false);
         Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(listUsers.Meta));
         return Ok(listUsers);
+    }
+   
+    [ValidateModel]
+    [RoleBasedAuthorization(RoleBased = RoleTypes.ADM)]
+    [HttpPut("{id}")]
+    public async Task<IActionResult> SetRole(int id, [FromBody] RoleToUpdateDTO dto)
+    {
+        string[] roleCanBeUpdated =  { RoleTypes.CUSR, RoleTypes.ORG };
+
+        if (!roleCanBeUpdated.Contains(dto.Role))
+            return UnprocessableEntity(new Response(ResponseStatusEnum.Fail, "", "Can not update admin role", 422));
+        try
+        {
+                     
+            var entity = await _repoManager.UserMeta.GetRoleByUserIdAsync(id, true);
+                    
+            if (entity == null) throw new ArgumentNullException();
+            _mapper.Map(dto, entity);
+            await _repoManager.SaveAsync();
+        
+            return Ok(new Response(ResponseStatusEnum.Success, entity, $"Update to {dto.Role} successfully", 200));
+        }
+        catch (ArgumentNullException)
+        {
+            return UnprocessableEntity(new Response(ResponseStatusEnum.Error, "", $"Id {id} is not existed", 422));
+        }
     }
 }
--- SEEMS/Data/Repositories/Implements/UserMetaRepository.cs
+++ SEEMS/Data/Repositories/Implements/UserMetaRepository.cs
@@ -29,4 +29,7 @@ public class UserMetaRepository : RepositoryBase<UserMeta>, IUserMetaRepository
         await FindByCondition(m => m.User.Email.Equals(email), trackChanges)
             .SingleOrDefaultAsync();
 
+    public async Task<UserMeta> GetRoleByUserIdAsync(int id, bool trackChanges) =>
+        await FindByCondition(m => m.UserId == id, trackChanges)
+            .SingleOrDefaultAsync();
 }
