892f9262fea939f89cdb990d6111a724af5fc58d
Trung Kien <kien123456k@gmail.com>
2022-03-07 23:25:31 +0700

    fix check attendance page error

--- SEEMS/Client/src/pages/Admin/CheckAttendance/index.js
+++ SEEMS/Client/src/pages/Admin/CheckAttendance/index.js
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import { useHistory, useParams } from 'react-router-dom'
 
 import { Typography, Box } from '@mui/material'
 
+import { useSnackbar } from '../../HOCs/SnackbarContext'
+import useEventAction from '../../recoil/event/action'
 import UserFilter from './UserFilter'
 import UserTable from './UserTable'
 
 const CheckAttendance = () => {
+    const { id } = useParams()
+    const history = useHistory()
+    const showSnackbar = useSnackbar()
     const [emailFilter, setEmailFilter] = useState('')
+    const { checkIsMyEvent } = useEventAction()
+
+    useEffect(() => {
+        checkIsMyEvent(id)
+            .then((res) => {
+                if (!res.data.data.isMine) {
+                    history.push('/admin')
+                }
+            })
+            .catch(() => {
+                showSnackbar({
+                    severity: 'error',
+                    children: 'Something went wrong, please try again later.',
+                })
+            })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <Box component="main" minHeight="65vh" mt={8.5} mb={10} mx={16} pt={8}>
