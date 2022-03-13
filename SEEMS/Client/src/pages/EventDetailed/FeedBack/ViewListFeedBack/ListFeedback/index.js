c41491710237bc4470b711e62921f78739a0be29
Hien-BT01 <hienbui25g@gmail.com>
2022-03-13 14:01:51 +0700

    SEEM-141 initiating implement load more for feedback

--- SEEMS/Client/src/pages/EventDetailed/FeedBack/ViewListFeedBack/ListFeedback/index.js
+++ SEEMS/Client/src/pages/EventDetailed/FeedBack/ViewListFeedBack/ListFeedback/index.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+import { Box, CircularProgress } from '@mui/material'
 
 import FeedbackItem from '../FeedbackItem'
 
+const Loading = () => (
+    <Box display="flex" justifyContent="center" my={3}>
+        <CircularProgress thickness={4} color="secondary" />
+    </Box>
+)
+
 const ListFeedback = ({ feedbacks }) => {
+    const [cloneFeedbacks, setCloneFeedbacks] = useState([])
+    const [hasMore, setHasMore] = useState(true)
+
+    const fakeFetchMore = () => {
+        if (cloneFeedbacks.length >= feedbacks.length) {
+            setHasMore(false)
+            return
+        }
+        setTimeout(() => {
+            setCloneFeedbacks((previousValue) => [...previousValue])
+        }, 1000)
+    }
+
     return (
         <React.Fragment>
-            {feedbacks.length !== 0 &&
-                feedbacks.map((feedback) => (
-                    <FeedbackItem
-                        key={feedback.id}
-                        content={feedback.content}
-                        rating={feedback.rating}
-                    />
-                ))}
+            {cloneFeedbacks.length ? (
+                <InfiniteScroll
+                    dataLength={cloneFeedbacks.length}
+                    loader={<Loading />}
+                    hasMore={hasMore}
+                    next={fakeFetchMore}
+                >
+                    {cloneFeedbacks.map((feedback) => (
+                        <FeedbackItem
+                            key={feedback.id}
+                            content={feedback.content}
+                            rating={feedback.rating}
+                        />
+                    ))}
+                </InfiniteScroll>
+            ) : (
+                <Box>There is no event available</Box>
+            )}
         </React.Fragment>
     )
 }
