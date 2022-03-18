e85ad294f7b6f3b6400d489f848db4fc8d9ffaa6
Trung Kien <kien123456k@gmail.com>
2022-03-18 08:23:16 +0700

    SEEM-147 add automatic logout for banned users

--- SEEMS/Client/src/recoil/event/action.js
+++ SEEMS/Client/src/recoil/event/action.js
@@ -1,18 +1,54 @@
 import { get, post, put } from '../../utils/ApiCaller'
+import useAuthAction from '../auth/action'
 
 const useEventAction = () => {
-    const getUpcomingEvents = () => get({ endpoint: '/api/events/upcoming' })
+    const { logout } = useAuthAction()
+    const getUpcomingEvents = () =>
+        get({ endpoint: '/api/events/upcoming' }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
-    const getEvents = (filterString) => get({ endpoint: '/api/events' + filterString })
+    const getEvents = (filterString) =>
+        get({ endpoint: '/api/events' + filterString }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
-    const getMyEvents = (filterString) => get({ endpoint: '/api/Events/my-events' + filterString })
+    const getMyEvents = (filterString) =>
+        get({ endpoint: '/api/Events/my-events' + filterString }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
-    const getDetailedEvent = (eventId) => get({ endpoint: `/api/events/${eventId}` })
+    const getDetailedEvent = (eventId) =>
+        get({ endpoint: `/api/events/${eventId}` }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
     const createEvent = (eventData) =>
         post({
             endpoint: '/api/events',
             body: eventData,
+        }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
         })
 
     const registerEvent = (eventId) =>
@@ -21,17 +57,49 @@ const useEventAction = () => {
             body: {
                 eventId,
             },
+        }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
         })
 
     const updateEvent = (eventId, eventData) =>
-        put({ endpoint: `/api/events/${eventId}`, body: eventData })
+        put({ endpoint: `/api/events/${eventId}`, body: eventData }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
-    const checkIsMyEvent = (id) => get({ endpoint: `/api/events/is-mine/${id}` })
+    const checkIsMyEvent = (id) =>
+        get({ endpoint: `/api/events/is-mine/${id}` }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
     const getMyRegistrations = (filterString) =>
-        get({ endpoint: '/api/reservations' + filterString })
+        get({ endpoint: '/api/reservations' + filterString }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
-    const checkCanAttendance = (id) => get({ endpoint: `/api/events/can-take-attendance/${id}` })
+    const checkCanAttendance = (id) =>
+        get({ endpoint: `/api/events/can-take-attendance/${id}` }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
     return {
         getUpcomingEvents,
--- SEEMS/Client/src/recoil/feedback/action.js
+++ SEEMS/Client/src/recoil/feedback/action.js
@@ -1,12 +1,34 @@
 import { get, post } from '../../utils/ApiCaller'
+import useAuthAction from '../auth/action'
 
 const useFeedbackAction = () => {
+    const { logout } = useAuthAction()
     const createFeedback = (feedbackContent) =>
-        post({ endpoint: '/api/feedbacks', body: feedbackContent })
+        post({ endpoint: '/api/feedbacks', body: feedbackContent }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
-    const checkCanFeedback = (eventId) => get({ endpoint: `/api/feedbacks/canFeedback/${eventId}` })
+    const checkCanFeedback = (eventId) =>
+        get({ endpoint: `/api/feedbacks/canFeedback/${eventId}` }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
-    const getFeedbacksOfEvent = (eventId) => post({ endpoint: `/api/feedbacks/${eventId}` })
+    const getFeedbacksOfEvent = (eventId) =>
+        post({ endpoint: `/api/feedbacks/${eventId}` }).then((res) => {
+            if (res.data.data && res.data.data.errorCode === 'BANNED_USER') {
+                logout()
+                window.location.reload(false)
+            }
+            return res
+        })
 
     return {
         createFeedback,
