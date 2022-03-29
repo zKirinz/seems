bd42aad1314f964f573eb97e642444225297b9ec
Hien-BT01 <hienbui25g@gmail.com>
2022-03-30 01:59:05 +0700

    SEEM-166 fixing some stuff

--- SEEMS/Controllers/AuthenticationController.cs
+++ SEEMS/Controllers/AuthenticationController.cs
@@ -72,8 +72,7 @@ public class AuthenticationController : ControllerBase
         {
             HttpOnly = true
         });
-        return Ok(accessToken);
-        // return Redirect($"{MapLoginUiDomain()}?token={accessToken}");
+        return Redirect($"{MapLoginUiDomain()}?token={accessToken}");
     }
 
     [HttpPost]
