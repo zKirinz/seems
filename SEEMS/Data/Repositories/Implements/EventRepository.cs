ceabf3e0d81f34010f68ca7909d06cf83df8c086
Thinh Le <letienthinh0109@gmail.com>
2022-04-01 23:18:14 +0700

    Fix canRegister method wrong result

--- SEEMS/Data/Repositories/Implements/EventRepository.cs
+++ SEEMS/Data/Repositories/Implements/EventRepository.cs
@@ -51,7 +51,7 @@ public class EventRepository : RepositoryBase<Event>, IEventRepository
         var myEvent = _context.Events.FirstOrDefault(e => e.Id == id);
         var registeredNum = _context.Reservations.Count(r => r.EventId == id);
         return myEvent.RegistrationDeadline.ToLocalTime().CompareTo(DateTime.Now) > 0 &&
-               (registeredNum == 0 || registeredNum < myEvent.ParticipantNum);
+               (myEvent.ParticipantNum == 0 || registeredNum < myEvent.ParticipantNum);
     }
 
     public bool CanUnregister(int id, int minHourToUnregister)
